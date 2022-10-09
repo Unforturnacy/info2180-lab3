2ed6a552cec5d81ee1df338e6669ada02c00f23c
Teric Simons <tericsimons12@gmail.com>
2022-10-09 18:53:20 -0500

    check for winner,update game status and hover

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,12 +1,25 @@
 var round = 1;
 var squares;
+var board;
 
 function newGame() {
     console.log("New Game Button Clicked");
      squares = document.getElementById('board').children;
+
+     board = [
+        [squares[0],squares[1],squares[2]],
+        [squares[3],squares[4],squares[5]],
+        [squares[6],squares[7],squares[8]]
+    ];
   
     for (let i = 0; i < squares.length; i++) {
         squares[i].classList.add('square');
+        squares[i].addEventListener("mouseover", function(){
+            squares[i].classList.add('hover');
+        });
+        squares[i].addEventListener("mouseleave", function(){
+            squares[i].classList.remove('hover');
+        });
         squares[i].addEventListener("click", function(){
             var square = squares[i];
             isvalid(square);
@@ -18,6 +31,7 @@ function newGame() {
 
   function isvalid(square)
   {
+    var id = document.getElementById("status");
     if(square.innerHTML == "X" || square.innerHTML == "O")
     {
         console.log("already taken");
@@ -28,14 +42,30 @@ function newGame() {
         {
             square.classList.add('O');
             square.innerHTML =  "O";
+            if(checkwin())
+            {
+                console.log("O WINS");
+                id.innerHTML = "Congratulations! O is the Winner!";
+                id.classList.add("you-won");
+            }
         }
         else
         {
-            square.classList.add('O');
+            square.classList.add('X');
             square.innerHTML =  "X";
+            if(checkwin())
+            {
+                console.log("X WINS");
+                id.innerHTML = "Congratulations! X is the Winner!";
+                id.classList.add("you-won");
+            }
         }
+
+      
         round+=1;
     }
+
+
 
     
   }
@@ -47,6 +77,86 @@ function move(square){
 }
 
 
+function checkwin()
+{
+    var win = false;
+    //check rows
+   for(let i = 0; i < 3;i++)
+   {
+        
+        if(win){console.log(win);return win;};
+        win = true;
+        for(let x = 0; x < 2;x++)
+        {
+            if (board[i][x].innerHTML =="" || board[i][x+1].innerHTML =="") 
+            {
+                win = false;
+                break;
+            }
+            if(board[i][x].innerHTML != board[i][x+1].innerHTML)
+            {
+                win = false;
+                break;
+            }
+
+            
+        }    
+   } 
+
+   //check col
+   for(let i = 0; i < 3;i++)
+   {
+        
+        if(win){console.log(win);return win;};
+        win = true;
+        for(let x = 0; x < 2;x++)
+        {
+            if (board[x][i].innerHTML =="" || board[x+1][i].innerHTML =="") 
+            {
+                win = false;
+                break;
+            }
+            if(board[x][i].innerHTML != board[x+1][i].innerHTML)
+            {
+                win = false;
+                break;
+            }
+
+            
+        }    
+   } 
+
+   //check diag
+   win = true;
+   for(let i = 0; i < 2;i++)
+   {
+           
+        if ((board[i][i].innerHTML =="" || board[i+1][i+1].innerHTML =="")) 
+        {
+            win = false;
+            break;
+        }
+        if(board[i][i].innerHTML != board[i+1][i+1].innerHTML)
+        {
+            win = false;
+            break;
+        }          
+    } 
+    
+    if(win){return win;}
+    win = false;
+
+   if((board[0][2].innerHTML == board[1][1].innerHTML)&&((board[1][1].innerHTML == board[2][0].innerHTML)))
+   {
+    if(board[0][2].innerHTML != "")
+    {
+        win = true;
+    }
+   }
+    console.log(win);
+    return win;
+   
+}
 
 
 
