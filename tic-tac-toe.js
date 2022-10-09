f6d1788bd3aa8245fce08e5658950076ebd661db
Teric Simons <tericsimons12@gmail.com>
2022-10-09 16:53:14 -0500

    alternate x and o

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,12 +6,45 @@ function newGame() {
      squares = document.getElementById('board').children;
   
     for (let i = 0; i < squares.length; i++) {
-        squares[i].classList.add('square');      
+        squares[i].classList.add('square');
+        squares[i].addEventListener("click", function(){
+            var square = squares[i];
+            isvalid(square);
+        });
+        
+      }
   }
+
+
+  function isvalid(square)
+  {
+    if(square.innerHTML == "X" || square.innerHTML == "O")
+    {
+        console.log("already taken");
+    }
+    else
+    {
+        if(round % 2 == 0)
+        {
+            square.classList.add('O');
+            square.innerHTML =  "O";
+        }
+        else
+        {
+            square.classList.add('O');
+            square.innerHTML =  "X";
+        }
+        round+=1;
+    }
+
+    
+  }
+
+function move(square){
+    console.log(square);
+
+
 }
-
-
-
 
 
 
